d63912ae52e2be3cb56abefa371d7558d6e45adf
Doa <doajafri@gmail.com>
2015-04-24 02:53:24 -0400

    cleaned app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -4,8 +4,7 @@ var MainComponent = require('./main-component.jsx')
 
 $.get('/chords').success(function (chords) {
   React.render(
-    // component we want to render (or straight html)
-    <MainComponent text="dudes" chords={chords}/>,
+    <MainComponent chords={chords}/>,
     document.body
   )
 })
